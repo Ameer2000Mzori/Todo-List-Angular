4084b09aa489efc8e19fbdef05e11e793ddf366a
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-30 23:27:19 +0100

    added todoData

---
+++ src/app/first-file/data.ts
@@ -0,0 +1,20 @@
+export const todoData = [
+  {
+    id: 1,
+    name: '<NAME>',
+    email: '<EMAIL>',
+    password: '<PASSWORD>',
+  },
+  {
+    id: 2,
+    name: '<NAME>',
+    email: '<EMAIL>',
+    password: '<PASSWORD>',
+  },
+  {
+    id: 3,
+    name: '<NAME>',
+    email: '<EMAIL>',
+    password: '<PASSWORD>',
+  },
+];
