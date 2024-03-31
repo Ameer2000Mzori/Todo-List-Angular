c1d745c0014aa07b7076be55921e6dc19f978d9d
Ameer Ameen <ameerapex496@gmail.com>
2024-03-31 11:26:48 +0200

    added few changes

--- src/app/first-file/child-file/garnd-child-file/garnd-child-file.component.ts
+++ src/app/first-file/child-file/garnd-child-file/garnd-child-file.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-garnd-child-file',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './garnd-child-file.component.html',
-  styleUrl: './garnd-child-file.component.scss'
+  styleUrl: './garnd-child-file.component.scss',
 })
 export class GarndChildFileComponent {
+  color = 'red';
 
+  checkColor(color: string) {
+    return color === 'red'
+      ? { backgroundColor: 'red' }
+      : { backgroundColor: 'blue' };
+  }
 }
